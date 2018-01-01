d832d1553713dfb9cb78745901eecacd3f2bddc5
Andy Anderson <andy@slowlanecafe.com>
2018-01-01 15:22:12 -0500

    Delete all code and dynamically create the game board and bomb board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,50 @@
 const printBoard = (board) => {
-    console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+    console.log(board.map( row => row.join(' | ')).join('\n'));
 };
 
-const board = [
-   [' ', ' ', ' '], 
-   [' ', ' ', ' '],
-   [' ', ' ', ' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (var rowNumber=0; rowNumber<numberOfRows; rowNumber++){
+        let row = [];
+        for (var colNumber=0; colNumber<numberOfColumns; colNumber++){
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+};
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (var rowNumber=0; rowNumber<numberOfRows; rowNumber++){
+        let row = [];
+        for (var colNumber=0; colNumber<numberOfColumns; colNumber++){
+            row.push(null);
+        }
+        board.push(row);
+    }
 
+    let numberOfBombsPlaced = 0;
+    let randomRowIndex = 0;
+    let randomColIndex = 0;
+    while(numberOfBombsPlaced < numberOfBombs){
+        // this loop has the potential to place bombs on top of bombs,
+        // but this will be fixed later...
+        randomRowIndex = Math.floor(Math.random() * numberOfRows);
+//        console.log('Random Row Index: ' + randomRowIndex);
+        randomColIndex = Math.floor(Math.random() * numberOfColumns);
+//        console.log('Random Col Index: ' + randomColIndex);
+        board[randomRowIndex][randomColIndex] = 'B';
+        numberOfBombsPlaced++;
+    };
+    return board;
+};
+
+const playerBoard = generatePlayerBoard(3,4);
+const bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+console.log('Bomb Board: ');
+printBoard(bombBoard);
